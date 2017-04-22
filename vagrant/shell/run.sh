a8c9f5701a4a6bd08dbb971e76537403f558a9cc
Marco Antonio Rocha <mrcrch@yahoo.com.br>
2017-04-22 02:00:56 -0300

    Ajustes de logging e profiles

--- vagrant/shell/run.sh
+++ vagrant/shell/run.sh
@@ -3,8 +3,7 @@
 fuser -v -n tcp 80 && fuser -k 80/tcp
 
 dir=/sigapi
-rm -rf ${dir}
 mkdir -p ${dir}
 cp /tmp/sigapi/coletor.jar ${dir}/
 
-nohup java -Dfile.encoding=UTF-8 -Dserver.port=80 -jar ${dir}/coletor.jar > ${dir}/coletor-nohup.out &
+nohup java -Dspring.profiles.active=prod -jar ${dir}/coletor.jar &
