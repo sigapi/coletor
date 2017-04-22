9c51e136fc04fcced9c270b667be0c9ce167030d
Marco Antonio Rocha <mrcrch@yahoo.com.br>
2017-04-22 02:59:14 -0300

    Ajustes nos scripts de execução

--- vagrant/shell/run.sh
+++ vagrant/shell/run.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-fuser -v -n tcp 80 && fuser -k 80/tcp
+# Para a execução anterior
+echo "Execução anterior..."
+fuser -k -TERM -n tcp 80 -s
 
+# Prepara o diretório
+echo "Diretório..."
 dir=/sigapi
 mkdir -p ${dir}
-cp /tmp/sigapi/coletor.jar ${dir}/
+cd ${dir}
+cp /tmp/sigapi/coletor.jar .
 
-nohup java -Dspring.profiles.active=prod -jar ${dir}/coletor.jar &
+# Salva cópia dos logs
+echo "Logs..."
+if [ -d logs ]; then
+    cd logs
+    if [ -f $FILE ]; then
+        cp coletor.{log,"$(date +%Y%m%d-%H%M%S)".log}
+        rm -rf coletor.log
+    fi
+fi
+
+# Iniciando execução atual
+echo "Iniciando..."
+cd ${dir}
+nohup java -Dspring.profiles.active=prod -jar coletor.jar &
+sleep 5s
